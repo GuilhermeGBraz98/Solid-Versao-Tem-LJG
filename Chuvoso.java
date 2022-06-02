a290f3c25cb3d2e7ae0f93474b9b49b30dda3e42
GuilhermeGBraz98 <106745897+GuilhermeGBraz98@users.noreply.github.com>
2022-06-02 10:07:24 -0300

    Delete Chuvoso.java

--- Chuvoso.java
+++
@@ -1,9 +0,0 @@
-package NotificaçõesClima;
-
-public class Chuvoso implements NotificacaoClima {
-
-    @Override
-    public void notificaClima() {
-        System.out.println("Esta chovendo, pegue um guarda-chuva");
-    }
-}
