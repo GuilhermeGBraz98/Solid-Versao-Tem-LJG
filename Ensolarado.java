2065d2d1a59e3c234e2173aa7cd0ccef13f7e485
GuilhermeGBraz98 <106745897+GuilhermeGBraz98@users.noreply.github.com>
2022-06-02 10:07:42 -0300

    Delete Ensolarado.java

--- Ensolarado.java
+++
@@ -1,9 +0,0 @@
-package NotificaçõesClima;
-
-public class Ensolarado implements NotificacaoClima {
-
-    public void notificaClima (){
-        System.out.println ("Esta muito Sol, partiu praia !");
-
-    }
-}
